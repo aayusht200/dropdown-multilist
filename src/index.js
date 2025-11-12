fd41581fa60b78b4613f54cb88ede398b6ee6290
aayusht200 <aayush.t200@gmail.com>
2025-11-12 20:04:40 +0530

    multilist done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './style.css';
+document.addEventListener('click', (e) => {
+    const isDropdown = e.target.matches('[data-dropdown-button]');
+    if (!isDropdown && e.target.closest('[data-dropdown]') !== null) return;
+    let currentDropdown;
+    if (isDropdown) {
+        currentDropdown = e.target.closest('[data-dropdown]');
+        currentDropdown.classList.add('active');
+    }
+    document.querySelectorAll('[data-dropdown].active').forEach((dropdown) => {
+        if (dropdown === currentDropdown) return;
+        dropdown.classList.remove('active');
+    });
+});
